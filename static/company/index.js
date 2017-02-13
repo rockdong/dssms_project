8ddb2ad6a4f4ec180485dac535778ec04a8c1683
rockdong <rockdong@1234567.local>
2017-02-13 21:37:14 +0800

    汉化主页菜单

--- static/company/index.js
+++ static/company/index.js
@@ -5,13 +5,23 @@
 $(function () {
     // 给每个一可点击跳转的菜单加载方法
     // 当前节点下有子节点
-    $("nav > ul.nav > li > ul > li > a").bind('click', function () {
+    $("nav > ul.nav > li > ul > li > a[href='#']").bind('click', function () {
         getHtmlCode($(this).attr("name"));
     });
     // 如果当前节点下面没有 ul 子节点，说明菜单项中没有子项
     $("nav > ul.nav > li > a:only-child").bind('click', function () {
         getHtmlCode($(this).attr("name"));
     });
+
+    // 给每个一可点击跳转的菜单加载方法
+    // 当前节点下有子节点
+    $("div[class='dropdown-menu wrapper w-full bg-white'] > div > div > div.row > div > ul > li > a[href='#']").bind('click', function () {
+        getHtmlCode($(this).attr("name"));
+    });
+    // // 如果当前节点下面没有 ul 子节点，说明菜单项中没有子项
+    // $("nav > ul.nav > li > a:only-child").bind('click', function () {
+    //     getHtmlCode($(this).attr("name"));
+    // });
 
 });
 
