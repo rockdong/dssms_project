0f3c85c14893d7ee0484f796ef2396642aa56999
rockdong <doufuhai@hotmail.com>
2017-03-07 23:32:49 +0800

    new

--- static/company/index.js
+++ static/company/index.js
@@ -46,11 +46,31 @@ menuApp.run(function () {
 //
 // });
 
+
+// $(function () {
+//     // 给每个一可点击跳转的菜单加载方法
+//     // 当前节点下有子节点
+//     $("nav > ul.nav > li > ul > li > a[href='#']").bind('click', function () {
+//         getHtmlCode($(this).attr("name"));
+//     });
+//     // 如果当前节点下面没有 ul 子节点，说明菜单项中没有子项
+//     $("nav > ul.nav > li > a:only-child").bind('click', function () {
+//         getHtmlCode($(this).attr("name"));
+//     });
+//
+//     // 给每个一可点击跳转的菜单加载方法
+//     // 当前节点下有子节点
+//     $("div[class='dropdown-menu wrapper w-full bg-white'] > div > div > div.row > div > ul > li > a[href='#']").bind('click', function () {
+//         getHtmlCode($(this).attr("name"));
+//     });
+//     // // 如果当前节点下面没有 ul 子节点，说明菜单项中没有子项
+//     // $("nav > ul.nav > li > a:only-child").bind('click', function () {
+//     //     getHtmlCode($(this).attr("name"));
+//     // });
+//
+// });
+
 function getHtmlCode(value) {
     $("#content").html("");
-<<<<<<< HEAD
-    $("#content").load(value);
-=======
     $("#content").load('/index/' + value);
->>>>>>> e276f3f7d16b1e767a88825a955af01da4e41916
 }
